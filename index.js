38a5a704a8da1e907eb4533682c8a9b6d1f48392
Mattia <matteron96@gmail.com>
2019-09-09 19:14:07 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const regex = /(\d+:\d+)/gi;
+
+const test = '[8:30 - 5:30] asdasd asdfa sdf [3:30 - 4:30]';
+
+var stdin = process.openStdin();
+
+function readTime(input) {
+	console.log('---------------------');
+	const times = input.toString().match(regex);
+
+	for(let i = 0; i < times.length; i += 2) {
+		let time1 = times[i].length < 5 ? '0' + times[i] : times[i];
+		let time2 = times[i+1].length < 5 ? '0' + times[i+1] : times[i+1];
+
+		let date1 = new Date(Date.parse('1996-04-12T' + time1));
+		let date2 = new Date(Date.parse('1996-04-12T' + time2));
+
+		
+
+		var hours = (date2 - date1) / 36e5;
+
+		if(hours < 0) {
+			date2.setHours( date2.getHours() + 12 );
+			var hours = (date2 - date1) / 36e5;
+		}
+
+		console.log('[' + time1 + ' - ' + time2 + '] = ', hours);
+	}
+}
+
+stdin.addListener("data", readTime);
