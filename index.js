26320ee05ca52c01b91cf7164c05a1c859242297
matteron <matteron96@gmail.com>
2019-09-12 15:56:16 -0400

    Now totals hours and looks for lunch entry to subtract from total

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-const stdin = process.openStdin();
+const readline = require('readline');
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+var buffer = [];
+
 const regex = /(\d+:\d+)/gi;
+const lunchEx = /(\[\d+:\d+ - \d+:\d+\] Lunch)/gi;
 
 function readTime(input) {
-	console.log('---------------------');
+	console.log('');
 	const times = input.toString().match(regex);
+	if (!times) {
+		return;
+	}
+
+	var total = 0;
 
 	for(let i = 0; i < times.length; i += 2) {
+		var hours = 0;
+		var isLunch = !lunchEx.test('[' + times[i] + ' - ' + times[i+1] + '] Lunch');
+		
 		let time1 = times[i].length < 5 ? '0' + times[i] : times[i];
 		let time2 = times[i+1].length < 5 ? '0' + times[i+1] : times[i+1];
 
 		let date1 = new Date(Date.parse('1996-04-12T' + time1));
 		let date2 = new Date(Date.parse('1996-04-12T' + time2));
 
-		var hours = (date2 - date1) / 36e5;
+		hours = (date2 - date1) / 36e5;
 
 		if(hours < 0) {
 			date2.setHours( date2.getHours() + 12 );
-			var hours = (date2 - date1) / 36e5;
+			hours = (date2 - date1) / 36e5;
 		}
 
-		console.log('[' + time1 + ' - ' + time2 + '] = ', hours);
+		total += (isLunch ? (-1 * hours) : hours);
 	}
+
+	console.log('Total: ' + total);
+	rl.prompt();
 }
 
-stdin.addListener("data", readTime);
+rl.on('line', (cmd) => {
+	buffer.push(cmd);
+});
+
+rl.on('close', (cmd) => {
+	readTime(buffer.join('\n'));
+});
+
+rl.prompt();
